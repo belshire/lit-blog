6b68d91bcceaa58da3934386e2fcb18354625800
Blake Elshire <blake.elshire@projekt202.com>
2021-11-03 10:26:17 -0500

    initial commit

---
+++ public/components/blog-post.d.ts
@@ -0,0 +1,7 @@
+import { LitElement } from "lit";
+export declare class BlogPost extends LitElement {
+    postTitle: string;
+    postBody: string;
+    render(): import("lit-html").TemplateResult<1>;
+}
+//# sourceMappingURL=blog-post.d.ts.map
---
+++ src/components/blog-post.ts
@@ -0,0 +1,18 @@
+import { LitElement, html } from "lit"
+import { customElement, property } from "lit/decorators"
+
+@customElement("blog-post")
+export class BlogPost extends LitElement {
+  @property()
+  postTitle = "My Title"
+
+  @property()
+  postBody = ""
+
+  override render() {
+    return html`
+      <h1>${this.postTitle}</h1>
+      <div>${this.postBody}</div>
+    `
+  }
+}
